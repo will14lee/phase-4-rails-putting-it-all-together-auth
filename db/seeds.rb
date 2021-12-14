289aa30b723a2914ee7419f95e6e9ff0e77d9111
will14lee <will14lee@gmail.com>
2021-12-14 02:00:54 -0500

    commit tree 696ed439de1255a1f3091e66bda418bdfa315185

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Recipe.create(
+    user_id: User.first.id,
+    title: "Rice", 
+    instructions: "Measure 1 cup of rice in bowl of rice cooker. Wash rice. Fill with water to level indicated by manufacturer. Put bowl in rice cooker. Press 'Cook'. Enjoy!",
+    minutes_to_complete: 10
+    )
